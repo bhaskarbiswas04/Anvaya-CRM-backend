4fc17a5b96e9dc3fdf877095c1a8738bff2aed93
BHASKAR BISWAS <73770735+bhaskarbiswas04@users.noreply.github.com>
2026-03-10 23:27:41 +0530

    updt: deployed api done

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ async function startServer() {
 
     // Default Route.
     app.get("/", (req, res) => {
-      res.send("🚀 Anvaya CRM Backend Running MMANAGEMNET"); 
+      res.send("🚀 Anvaya CRM Backend Running"); 
     });
 
     const PORT = process.env.PORT;
